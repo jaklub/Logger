9cc23246adb5dd0e806b2dfcbcc2fafc5a155b14
Jakob <jakob.lublin@gmail.com>
2015-01-25 03:02:49 +0100

    kont

--- modules/d3.logger.js
+++ modules/d3.logger.js
@@ -15,17 +15,12 @@
         self.messages = [];
         self.init = function (options) {
             //self.options = options;
-            d3.select("body").append('div').attr('id', 'logger').attr('style', "position: absolute; bottom: 10px; width: 80%;").append('span').classed('log-wrapper hidden', true).attr('style', 'opacity: 0; background-color: #555555; display: block; height: 17px; overflow: hidden; border-radius: 10px;padding: 10px; color: white;');
+            d3.select("body").append('div').attr('id', 'logger').attr('style', "position: absolute; bottom: 10px; width: 80%;").append('span').classed('log-wrapper hidden', true).attr('style', 'opacity: 0; background-color: #555555; display: block; height: 17px; overflow: hidden; border-radius: 10px; padding-top: 10px; padding-bottom: 10px;; color: white;');
             //self.logElement = self.d3.select("#logger > .log-wrapper");
             self.logElement = self.d3.select("#logger");
             self.wrapper = self.d3.select("#logger").select('.log-wrapper');
             self.logElement.on('click', function (arg1, arg2) {
-                if (!self.wrapper.classed('expanded')) {
-                    self.wrapper.classed('expanded', true).transition().style('height', '250px').style('overflow', 'auto');
-                }
-                else {
-                    self.wrapper.classed('expanded', false).transition().style('height', '17px').style('overflow', 'hidden').style('overflow', 'auto');
-                }
+                toggleExpand(self.wrapper);
             });
             self.logElement.on('mouseover', function (arg1, arg2) {
                 //console.log("mouse over");
@@ -47,11 +42,37 @@
             }
             self.messages.push(message);
             var log = self.logElement.select('.log-wrapper').selectAll('div').data(self.messages);
-            var newMess = log.enter().insert('div', ':first-child').classed('message-wrapper', true).style({ 'margin-bottom': '10px', 'margin-top': '-28px' }).append('span').classed('message', true).text(function (d) {
+            var newMess = log.enter().insert('div', ':first-child').classed('message-wrapper', true).style({ 'margin-bottom': '0px', 'margin-top': '-28px', 'padding-top': '10px', 'padding-bottom': '10px', 'padding-left': '10px', 'background-color': 'rgb(85, 85, 85)' }).style('background-color', function (d, i) {
+                console.log(i);
+                if (i % 2 == 0 && self.wrapper.classed('expanded')) {
+                    return "rgb(80, 80, 80)";
+                }
+                else {
+                    return "rgb(85, 85, 85)";
+                }
+            }).append('span').classed('message', true).text(function (d) {
                 return d;
             });
             newMess.transition().style('margin', '1px');
             self.logElement.selectAll('.message-wrapper').transition().style('margin-top', '0px');
+        }
+        function toggleExpand(wrapper) {
+            if (!wrapper.classed('expanded')) {
+                wrapper.classed('expanded', true).transition().style('height', '250px').style('overflow', 'auto');
+                wrapper.selectAll('.message-wrapper').transition().duration(500).style('background-color', function (d, i) {
+                    var a = "";
+                    if (i % 2 == 0) {
+                        return "rgb(80, 80, 80)";
+                    }
+                    else {
+                        return "rgb(85, 85, 85)";
+                    }
+                });
+            }
+            else {
+                wrapper.classed('expanded', false).transition().style('height', '17px').style('overflow', 'hidden').style('overflow', 'auto');
+                wrapper.selectAll('.message-wrapper').transition().style('background-color', 'rgb(85, 85, 85)');
+            }
         }
     };
     return new Logger(d3);
--- modules/d3.logger.ts
+++ modules/d3.logger.ts
@@ -20,7 +20,7 @@ interface iLogMessage {
 interface iLog {
     d3: D3.Base;
     options: iLogOptions;
-    messages: [string];
+    messages: string[];
     logElement: D3.Selection;
     wrapper: D3.Selection;
     init(iLoggerOptions):  void;
@@ -44,7 +44,7 @@ interface iLog {
         var self: iLog  = <any>this;
         self.options = {loggerStyle: "background-color: aqua;"};
         self.d3 = d3;
-        self.messages = <[string]>[];
+        self.messages = [];
 
         self.init = function(options: iLogOptions){
             //self.options = options;
@@ -54,18 +54,13 @@ interface iLog {
                 .attr('style',"position: absolute; bottom: 10px; width: 80%;")
                 .append('span')
                 .classed('log-wrapper hidden', true)
-                .attr('style','opacity: 0; background-color: #555555; display: block; height: 17px; overflow: hidden; border-radius: 10px;padding: 10px; color: white;');
+                .attr('style','opacity: 0; background-color: #555555; display: block; height: 17px; overflow: hidden; border-radius: 10px; padding-top: 10px; padding-bottom: 10px;; color: white;');
 
             //self.logElement = self.d3.select("#logger > .log-wrapper");
             self.logElement = self.d3.select("#logger");
             self.wrapper = self.d3.select("#logger").select('.log-wrapper');
             self.logElement.on('click',function(arg1,arg2){
-                if(!self.wrapper.classed('expanded')){
-                    self.wrapper.classed('expanded',true).transition().style('height','250px').style('overflow','auto');
-                }else{
-                    self.wrapper.classed('expanded',false).transition().style('height','17px').style('overflow','hidden').style('overflow','auto');
-                }
-
+                toggleExpand(self.wrapper);
             });
 
             self.logElement.on('mouseover',function(arg1,arg2){
@@ -77,9 +72,6 @@ interface iLog {
                 }
 
             });
-
-
-
         };
 
         self.logMessage = function(message : string){
@@ -102,7 +94,15 @@ interface iLog {
             var newMess = log.enter()
                 .insert('div', ':first-child')
                 .classed('message-wrapper', true)
-                .style({'margin-bottom': '10px','margin-top': '-28px'})
+                .style({'margin-bottom': '0px','margin-top': '-28px', 'padding-top': '10px','padding-bottom': '10px', 'padding-left': '10px', 'background-color':'rgb(85, 85, 85)'})
+                .style('background-color',function(d,i) {
+                    console.log(i);
+                    if (i % 2 == 0 && self.wrapper.classed('expanded')) {
+                        return "rgb(80, 80, 80)";
+                    } else {
+                        return "rgb(85, 85, 85)";
+                    }
+                })
                 .append('span')
                 .classed('message', true)
                 .text(function(d){
@@ -118,8 +118,28 @@ interface iLog {
                 .style('margin-top', '0px');
 
 
+
         }
 
-    }
+
+        function toggleExpand(wrapper){
+            if(!wrapper.classed('expanded')){ // Expand
+                wrapper.classed('expanded',true).transition().style('height','250px').style('overflow','auto');
+                wrapper.selectAll('.message-wrapper').transition().duration(500).style('background-color',function(d,i){
+                    var a = "";
+                    if(i % 2 == 0){
+                        return "rgb(80, 80, 80)";
+                    }else{
+                        return "rgb(85, 85, 85)";
+                    }
+                });
+            }else{ // Collapse
+                wrapper.classed('expanded',false).transition().style('height','17px').style('overflow','hidden').style('overflow','auto');
+                wrapper.selectAll('.message-wrapper').transition().style('background-color', 'rgb(85, 85, 85)');
+            }
+
+        }
+
+    };
     return new Logger(d3);
 }));

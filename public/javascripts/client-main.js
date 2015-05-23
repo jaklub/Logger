bed2cb3bc909faa341da3c6b1a191018793301d9
Jakob <jakob.lublin@gmail.com>
2015-05-23 02:48:36 +0200

    kont

--- public/javascripts/client-main.js
+++ public/javascripts/client-main.js
@@ -9,8 +9,13 @@ define(["jquery", "d3", "d3logger", "test"], function($, d3, log, test) {
     var i = 1
 
     function logClient(arg1,arg2){
+
+        var temp = [{name:"foo1", label:"bar1"},{name:"foo2", label:"bar2"}];
+        var temp2 = JSON.stringify(temp,null,2);
+
         console.log("logClient");
-        log.logMessage("Log message " + i.toString());
+        //log.logMessage("Log message " + i.toString());
+        log.logMessage("Log message " + temp2);
         i = i +1;
     };
 
